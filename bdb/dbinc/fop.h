15b2ac2d036e49190fa0d533de26d823d85c168e
cian chambliss <cian@alphasoftware.com>
2024-12-27 13:02:50 -0500

    Initial commit

---
+++ bdb/dbinc/fop.h
@@ -0,0 +1,16 @@
+/*-
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2001-2002
+ *	Sleepycat Software.  All rights reserved.
+ *
+ * $Id: fop.h,v 1.1 2006/01/28 00:09:27 kurt Exp $
+ */
+
+#ifndef	_FOP_H_
+#define	_FOP_H_
+
+#include "dbinc_auto/fileops_auto.h"
+#include "dbinc_auto/fileops_ext.h"
+
+#endif /* !_FOP_H_ */
